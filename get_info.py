31010d4ea5fac56ed78ab6f9c7e0c846d5d3b852
emanuel alkobi <emanuelalkobi@MacBook-Pro.local>
2018-11-08 12:17:19 -0500

    update code based on the project review rejects

--- get_info.py
+++ get_info.py
@@ -4,15 +4,18 @@ from os.path import isfile
 def get_video_file(filename):
     assert isfile(filename) and access(filename, R_OK),"File {} doesn't exist or isn't readable".format(filename)
     return(filename)
+
 def get_name(i):
     team_name = input('Choose team number'+str(i)+' name :\n ')
     return team_name
 
 def get_color(i,colors):
-    print("get_color!!!!",colors)
-    color1=-1
-    color1 = input('Choose team number'+str(i)+' color(enter a number):\n1.red\n2.yellow\n3.green\n4.blue\n ')
-    color1=int(color1)
+    color1 = None
+    while color1 is None:
+        try:
+            color1 = int(input('Choose team number'+str(i)+' color(enter a number):\n1.red\n2.yellow\n3.green\n4.blue\n '))
+        except ValueError:
+            print("Program halted incorrect data entered,please Enter a number ")
     while (color1<0 and color1> len(colors)):
          print("you insert an unsupported color please insert another color from the list\n")
          color1 = input('Choose team number'+str(i)+' color(enter a number):\n1.red\n2.yellow\n3.green\n4.blue\n ')
